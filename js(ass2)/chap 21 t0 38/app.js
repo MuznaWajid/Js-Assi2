7e0ebb4cca2b0619564f6628a4a3480fdd032610
Muzna Ali <Muzna.wajidali@gmail.com>
2020-06-24 00:02:41 +0500

    JsAssing2

---
+++ js(ass2)/chap 21 t0 38/app.js
@@ -0,0 +1,373 @@
+// // // Question 01
+// // // var first_name = prompt("Enter Your First Name ");
+// // // var Last_name = prompt("Enter Your Last Name ");
+// // // var full_name = first_name + Last_name;
+// // // document.write("" + full_name + " Welcome to our js land:");
+// // // // Question 02
+// // // var mobil_brand = prompt("Enter Your fav mobile Model");
+// // // document.write("length of text is:", mobil_brand.length);
+// // // Question 03
+// // // var word = "Pakistani";
+// // // document.write("String is: \n" + word + "\n index of n is : ", word.indexOf("n"));
+// // // Question 04
+// // // var word = "Hello World";
+// // // console.log("String is: \n" + word + "\n index of l is : ", word.lastIndexOf("l"));
+// // // Question 05
+// // // var word = "Pakistani";
+// // // document.write("Characrter of index is: \n" + word + "\n index of i is : ", word.indexOf("i"));
+// // // Question 06
+// // // var word = "Hydrabad";
+// // // document.write("City : " + word + "\n After replace is : ", word.replace("Hydrabad", "Islamabad"));
+// // // Question 07
+// // // var message = ("Ali and Sami are best friends. They play cricket and football together.");
+// // // document.write(" : " + message + "\n After replace is : ", message.replace("and", "&"));
+// // // Question08
+// // // var integerString = "472";
+// // // document.write("value \n" + integerString + "\n type : string \n  value", Number(integerString), " \n type :Number");
+// // // Question 09
+// // // var user_input = "peanuts"
+// // // document.write(" User input " + user_input + " UPPER CASE ", user_input.toUpperCase());
+// // // Question 10
+// // // var user_input = "javascript"
+// // // document.write(" User input " + user_input + " Title case ", user_input.toLocaleLowerCase());
+// // // Question 11
+// // // var num = "35.36";
+// // // document.write("before : " + num + "\n After replace is : ", num.replace("35.36", "3536"));
+// // // Question 12
+// // // Question 13 Chap 26 to 30
+// // // var num = prompt("Please Enter a positive integer");
+// // // var round_off = Math.round(num)
+// // // var floor_value = Math.floor(num)
+// // // var ceil_valu = Math.ceil(num)
+// // // document.write("number" + num + "<br> round off value is:" + round_off + "<br/> Floor value" + floor_value + "<br/>Ceil value" + ceil_valu + "");
+// // // Question 14
+// // // var num = prompt("Please Enter a Negitive integer");
+// // // var round_off = Math.round(num)
+// // // var floor_value = Math.floor(num)
+// // // var ceil_valu = Math.ceil(num)
+// // // document.write("number" + num + "<br> round off value is:" + round_off + "<br/> Floor value" + floor_value + "<br/>Ceil value" + ceil_valu + "");
+// // // Question 15
+// // // var absolute_number = prompt("Enter any number");
+// // // document.write("The number is absolute", Math.abs(absolute_number));
+// // // Question 16
+// // // var random_die = prompt("Enter any random die number");
+// // // var random = Math.random();
+// // // var ran = (random * 6) + 1;
+// // // var die = Math.floor(ran);
+// // // document.write("Random die value is :" + die + "");
+// // // Question 17
+// // // var random_coin = prompt("Enter any random Coin number");
+// // // var random = Math.random();
+// // // var ran = (random * 2) + 1;
+// // // var Coin = Math.floor(ran);
+// // // if (Coin === 1) {
+// // //     Coin = "Head"
+// // // } else
+// // //     Coin = "Tail";
+// // // document.write("Random Coin value is :" + Coin + "");
+// // // Question 18
+// // // var random_num = prompt("Enter A Random Number");
+// // // var random = Math.random() * 100;
+// // // var Numb = Math.round(random);
+// // // document.write("Random number value is :" + Numb + "");
+// // // Question 19
+// // // var random_num = prompt("Enter A Random Number between 1 to 100");
+// // // var num=Math.floor(Math.random() * 100)+1;
+// // // document.write("Random number value is :" + num + "");
+// // // Question 20
+// // // var random_num = prompt("Enter Your Weight In Kilograms");
+
+// // // Question 21
+// // // var secrte_number = 9;
+// // // var user_input = +prompt("Enter A  Number between 1 to 10");
+// // // if (user_input === secrte_number) {
+// // //   alert("COngratulation Yahoo Yepppee:)!");
+// // // } else alert("Sorry Try Again Better Luck Next Time :(!");
+// // // Question 22 chap 31 to 34
+// // // var today = new Date();
+// // // document.write(today);
+// // // Question 23
+// // // var today = new Date();
+// // // var month =today.getMonth();
+// // // document.write("Current Month "+ month +" ");
+// // // Question 24
+// // // var user_input = prompt("Enter A Holidy Day");
+// // // var day = "Saturday";
+// // // var day1 = "Sunday";
+// // // if (user_input=== day){alert("Sat");}
+// // // else if(user_input===day1){alert("Sun");}
+// // // else {alert("No holiday Enjoy Your  Working today ;)")}
+// // // Question 25
+// // // var user_input = prompt("Enter A Holidy Day");
+// // //  var day = "Saturday";
+// // // var day1 = "Sunday";
+// // // if (user_input=== day){alert("Its funday");}
+// // // else if(user_input===day1){alert("Another Its funday");}
+// // // else {alert("No holiday Enjoy Your  Working today ;)")}
+// // // Question 26
+// // // var user_input = +prompt("Enter A Holidy Day");
+// // // var today = new Date();
+// // // var day =today.getDate();
+// // // if (day < 16){alert("First Fifteen days of the month");}
+// // // else {alert("Last Fifteen days of the month ;)");}
+// // // Question 27
+// // // var current = new Date();var milisecond = current.getTime();var minitues = milisecond/6000;
+// // // document.write("Current :" +current+ 
+// // // "<br/> Elapsed Milisecond Since January 1, 1970"
+// // // +milisecond+"<br/> Elapsed Milisecond Since January 1, 1970"+minitues+"" )
+// // // Question 28
+// // // var today = new Date();
+// // // var hours =today.getHours();
+// // // if (hours < 12){alert("It's AM ");}
+// // // else {alert("It's pM ;)");}
+// // // Question 29
+// // // var later_date = new Date("Thur Dec 31 2020")
+// // // document.write("Later Date:" +later_date+"");
+// // // Question 30
+// // // var d = new Date();
+// // // var later_date = new Date("June 18 2015");
+// // // var md = d.getTime();
+// // // var mlater_date = later_date.getTime();
+// // // var dif = md - mlater_date;
+// // // var db = dif/ (1000 * 60 * 60 * 24);
+// // // db =Math.floor(db);
+// // // alert(db +"Days have passed Since Ramadan");
+// // // Question31
+
+
+// var today = new Date();
+// var oldDate = new Date("January 1, 2015");
+// var msToday = today.getTime();
+// var msOldDate = oldDate.getTime();
+// var msDiff = msToday - msOldDate;
+// var dDiff = msToday - msOldDate;
+// var dDiff = msDiff / 1000 ;
+// dDIFF = Math.floor(dDiff);
+// document.write("on reference date" + today +"," + dDiff + "seconds had passed  since beginning of 2015");
+
+
+// var today = new Date();
+// document.write("Current Date: " + today + "<br/>");
+// var oneHourAgo = today.setHours(14);
+// document.write("1 hour ago, it was" +today);
+
+
+
+// var today = new Date();
+// document.write("current date: " + today + "<br/>");
+// var yearsAgo = today.setFullYear(1920);
+// document.write("100 years back, it was " + today);
+
+
+
+// // 13. Write a program to ask the user about his age. Calculate
+// // and show his birth year in your browser.
+
+// var userAge = prompt("Enter your age");
+// var birthYear = 2019 - userAge;
+// document.write("Your age is " + userAge + "<br/>");
+// document.write("Your birth year is " +birthYear);
+
+
+
+// var CustomerNam = "ABC Customer";
+// var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December" ];
+// var today = new Date();
+// var theDay = today.getMonth();
+// var nameOfMonth = monthNames[theDay-1];
+// var numUnits = 410;
+// var chargeUnit = 16;
+// var netAmount = numUnits * chargeUnit;
+// var latePayment = 350;
+// var grossAmount = netAmount + chargeUnit;
+
+// document.write("<h1>K-Electric Bill</h1>");
+// document.write("Customer Name: <b>ABC Customer</b><br />");
+// document.write("Month: " + "<b>" + nameOfMonth + "</b><br />");
+// document.write("Number of units: "+ "<b>" + numUnits + "</b><br />");
+// document.write("Charges per unit: "+ "<b>" + chargeUnit + "</b><br />");
+// document.write("Net amount payable (within due date): "+ "<b>" + netAmount + "</b><br />");
+// document.write("Late payment surcharge: "+ "<b>" + latePayment+ "</b><br />");
+// document.write("Gross amount payable (after due date): "+ "<b>" + grossAmount + "</b><br />");
+
+
+// // // Question 35
+// // // function tellTime(){
+// // // var d = new Date();
+// // // alert(d);
+// // // }
+// // // tellTime();
+// // // Question 36
+// // // function name(){
+// // //     var User_name = prompt("Enter Your Full Name");
+// // //     alert("Welcome To Our Js Hub " + User_name +" ");
+// // // }
+// // // name();
+// // // Question 37
+// // // function Sum_Num(){
+// // //     var Number1 = +prompt("Enter Your first Number")
+// // //     var Number2 = +prompt("Enter Your second Number");
+// // //     var sum= Number1+Number2 ;
+// // //   alert(sum);
+// // // }
+// // // Sum_Num();
+// // // Question 38
+// // // function calc(num1,num2){
+// // // var a =  num1;
+// // // var b =  num2;
+// // // document.write(a + b);
+// // // }
+// // // calc(5,5);
+// // // Question 39
+// // // function squ (num){
+// // // var Square = Math.pow(num,2)
+// // // document.write(Square);
+// // // }
+// // // squ(6);
+// // // 
+
+// // function factorial(num) {
+// //     if (num === 0) {
+// //         return 1;
+// //     }
+// //     else return num * factorial(num - 1);
+// // }
+// // document.write(factorial(5));
+
+
+
+// // function counting(start, end) {
+// //     var start =prompt("Enter start number");
+// //     var end = prompt("Enter end number");
+// //     var array = [];
+// //     for (var i = start; i <= end; i++){
+// //         document.write(array.push(i));
+
+// //     }
+// //     return array;
+// // }
+// // counting();
+
+
+// // // Hypotenuse 2 = Base 2 + Perpendicular 2
+// // // Take base and perpendicular as inputs.
+// // // Outer function : calculateHypotenuse()
+// // // Inner function: calculateSquare()
+
+// // function calculateHypotenuse(base, perp) {
+// // base = prompt("Enter base length");
+// // perp = prompt("Perpendicular height");
+// // return Math.sqrt(Math.pow(base, 2) + Math.pow(perp, 2));
+
+// // }
+
+// // document.write("Hypotenuse of triangle is " +calculateHypotenuse());
+
+
+// // var length = prompt("Enter the length of rectangle");
+// // var width = prompt("Enter the width of rectangle");
+
+// // function area(length,  width) {
+// //     return length * width;
+// // }
+// // document.write("The area of rectangle is " + area(length, width));
+
+
+// // // function palindrome(str){
+// // //     var cstr = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+// // //     var count= 0;
+// // //     if(cstr === ""){
+// // //         console.log("nothing found!");
+// // //         return false;
+// // //     }
+// // //   if ((cstr.length) % 2 === 0){
+// // //       count= (cstr.length) /2;
+
+// // //   }else {
+// // //       if ( cstr.length === 1){
+// // //           console.log(str + "is a palindrome.");
+// // //           return true;
+// // //       }else {
+// // //           count = (cstr.length -1) / 2;
+// // //       }
+// // //   }
+
+// // // for (var i = 0; i < count;i++ ) {
+// // //     if (cstr[i] != cstr.slice(-1-i)[0]){
+// // //         console.log(str + "is not a palindrome");
+// // //         return false;
+// // //     }
+// // // }
+// // // console.log(str + "is a palindrome");
+// // // return true;
+// // // }
+
+// // // palindrome("papa")
+
+
+
+// // // function upperCase(str) {
+// // //     var splitStr = str.toLowerCase().split(' ');
+// // //     for (var i = 0; i < splitStr.length; i++){
+// // //     splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring
+// // //     (1);
+// // //     }
+// // //     return splitStr.join(' ');
+
+// // // }
+
+// // // document.write("STRING : 'the quick brown fox' <br>");
+// // // document.write("OUTPUT: " + upperCase("the quick brown fox"));
+
+
+
+// // // function longestWord(str) {
+// // //     var array = str.match(/\w[a-z]{0,}/gi);
+// // //     var result = array[0];
+
+// // //     for(var i =1; i < array.length; i++) {
+// // //         if(result.length < array[i].length){
+// // //             result = array[i];
+// // //         }
+// // //     }
+// // //     return result
+// // // }
+
+// // document.write("String: web development tutorial<br>");
+// // document.write("The longest word: " + longestWord('web development tutorial'));
+
+
+
+
+
+// // // function charCount(str, letter) {
+// // //     var letterCount = 0;
+// // //     for(var i = 0; i < str.length; i++){
+// // //         if(str.charAt(i) == letter) {
+// // //             letterCount += 1;
+// // //         }
+// // //     } 
+// // //     return letterCount;
+// // // }
+
+// // // document.write("string: JSResourceS.com <br> ")
+// // // document.write("'o' occurs " + charCount("JSResources.com", "o") + "times within the string");
+
+
+
+
+
+
+
+// // // function calcCircumference(radius) {
+// // //     var circumference =( Math.PI*2)*radius;
+// // //     console.log("The circumference is " + circumference);
+// // // }
+
+// // // function calcArea(radius) {
+// // //     var area = Math.PI * radius * radius;
+// // //     console.log("The area is " + area);
+// // // }
+
+// // // calcCircumference(5);
+// // calcArea(5);
